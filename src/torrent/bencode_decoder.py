3bc0c7109d3df2dd47fc791a54888625e307577d
SaudKhanAbbas <saudkhanabbas@gmail.com>
2026-06-04 12:14:58 +0530

    Add automatic bencode type detection

--- src/torrent/bencode_decoder.py
+++ src/torrent/bencode_decoder.py
@@ -4,6 +4,17 @@ class BencodeDecoder:
         self.data = data
         self.index = 0
 
+    def decode(self):
+        current_char = self.data[self.index]
+
+        if current_char == "i":
+            return self.decode_integer()
+
+        if current_char.isdigit():
+            return self.decode_string()
+
+        raise ValueError(f"Unsupported type: {current_char}")
+
     def decode_integer(self):
         self.index += 1
 
@@ -15,9 +26,22 @@ class BencodeDecoder:
 
         return number
 
+    def decode_string(self):
+        colon_index = self.data.index(":", self.index)
 
-decoder = BencodeDecoder("i42e")
+        length = int(self.data[self.index:colon_index])
 
-result = decoder.decode_integer()
+        self.index = colon_index + 1
 
-print(result)
+        result = self.data[self.index:self.index + length]
+
+        self.index += length
+
+        return result
+
+
+decoder1 = BencodeDecoder("i42e")
+print(decoder1.decode())
+
+decoder2 = BencodeDecoder("5:hello")
+print(decoder2.decode())
